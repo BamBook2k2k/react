96f3159aed63b4e9b292b388db39fecb95ef6b27
Дмитрий Наумов <naumov@35a.ru>
2022-06-14 14:37:55 +0300

    добавка 14.06

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/counter";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 import './styles/App.css';
 
 function App() {
@@ -12,8 +14,34 @@ function App() {
     {id:3, title: 'JavaScript 3', body: 'Description'},
   ])
 
+  const [title, setTitle] = useState('')
+
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
+    console.log(bodyInputRef.current)
+  }
+
   return (
     <div className="App">
+      <form>
+        {/*Управляемый компонент*/}
+        <MyInput
+          value = {title}
+          onChange = {e => setTitle(e.target.value)}
+          type ="text" 
+          placeholder = "название поста"
+        />
+        {/*неуправляемый компонент*/}
+        <MyInput
+          ref = {bodyInputRef}
+          type ="text" 
+          placeholder = "описание поста"
+        />
+        <MyButton onClick = {addNewPost} >Создать пост</MyButton>
+      </form>
       <PostList posts = {posts} title = "список постов 1"/>
     </div>
   );
