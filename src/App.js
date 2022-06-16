02477bf986563b8d7b1daa8448277dffeaf82b54
Дмитрий Наумов <naumov@35a.ru>
2022-06-16 15:55:17 +0300

    сортировка и поиск

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/counter";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css';
 
 function App() {
   const [posts, setPosts] = useState ([
-    {id:1, title: 'JavaScript', body: 'Description'},
-    {id:2, title: 'JavaScript 2', body: 'Description'},
-    {id:3, title: 'JavaScript 3', body: 'Description'},
+    {id:1, title: 'qw', body: '12'},
+    {id:2, title: 'as', body: '34'},
+    {id:3, title: 'zx', body: '56'},
   ])
+
+  const [filter, setFilter] = useState ({sort: '', query: ''})
+
+  const sortedPosts = useMemo (() => {
+    if(filter.sort) {
+      return [...posts].sort((a, b,) => a[filter.sort].localeCompare(b[filter.sort]))
+    }
+    return posts;
+  },[filter.sort, posts])
+
+  const sortedAndSearchedPosts = useMemo (() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+  },[filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -27,7 +42,12 @@ function App() {
   return (
     <div className="App">
       <PostForm create = {createPost}/>
-      <PostList remove = {removePost} posts = {posts} title = "список постов 1"/>
+      <hr style = {{margin: '15px 0'}}/>
+      <PostFilter 
+        filter = {filter}
+        setFilter = {setFilter}
+      />
+      <PostList remove = {removePost} posts = {sortedAndSearchedPosts} title = "список постов 1"/>
     </div>
   );
 
